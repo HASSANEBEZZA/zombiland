468aa9808392bf2306c193553a1f70fe7ed9950c
Hassane <bezzahassane@gmail.com>
2024-08-24 12:24:00 +0200

    change database.js

--- app/config/database.js
+++ app/config/database.js
@@ -7,12 +7,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // Création d'une instance Sequelize
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
- 
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432, // Par défaut, PostgreSQL utilise le port 5432
   dialect: "postgres",
   protocol: "postgres",
   logging: false
- 
 });
 
 // Export de notre client sequelize
