506231be2c0e78fd0261155d4e39bf9e4e85c0b1
Hassane <bezzahassane@gmail.com>
2024-08-24 15:21:37 +0200

    server

--- app/config/redisClient.js
+++ app/config/redisClient.js
@@ -3,22 +3,19 @@ const redisHost = process.env.REDIS_HOST || 'localhost';
 const redisPort = process.env.REDIS_PORT || '6379';
 const redisPassword = process.env.REDIS_PASSWORD || '';
 
-// Construire l'URL Redis en fonction des paramètres
+// Construire l'URL Redis avec le protocole non-TLS
 let redisUrl = `redis://${redisHost}:${redisPort}`;
 if (redisPassword) {
   redisUrl = `redis://:${redisPassword}@${redisHost}:${redisPort}`;
 }
 
-// Création du client Redis sans TLS
+// Créer le client Redis sans TLS
 const redisClient = Redis.createClient({
-  url: redisUrl,
-  socket: {
-    // TLS est désactivé
-  }
+  url: redisUrl
 });
 
-// Gestion des erreurs de connexion Redis
-redisClient.on('error', (err) => console.error('Erreur de connexion Redis server :', err));
+// Gérer les erreurs de connexion Redis
+redisClient.on('error', (err) => console.error('Erreur de connexion Redis :', err));
 
 // Connexion à Redis
 redisClient.connect().catch(console.error);
