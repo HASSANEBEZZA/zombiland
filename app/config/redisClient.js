ee2c09f8379868ec9f00a7aeb58bd47b0765070c
Hassane <bezzahassane@gmail.com>
2024-08-24 14:00:34 +0200

    change redisClinet

--- app/config/redisClient.js
+++ app/config/redisClient.js
@@ -1,26 +1,23 @@
-const redis = require('redis');
-
-// Définir les variables Redis à partir des variables d'environnement
+const Redis = require('redis');
 const redisHost = process.env.REDIS_HOST || 'localhost';
 const redisPort = process.env.REDIS_PORT || '6379';
 const redisPassword = process.env.REDIS_PASSWORD || '';
 
-// Configuration de Redis sans TLS
-const redisClient = redis.createClient({
-  url: `redis://:${redisPassword}@${redisHost}:${redisPort}`
-});
+let redisUrl = `redis://`;
 
-redisClient.on('error', (err) => {
-  console.error('Erreur de connexion Redis :', err);
-});
+if (redisPassword) {
+  redisUrl += `:${redisPassword}@`;
+}
 
-(async () => {
-  try {
-    await redisClient.connect();
-    console.log('Connecté à Redis avec succès');
-  } catch (err) {
-    console.error('Erreur lors de la connexion à Redis:', err);
+redisUrl += `${redisHost}:${redisPort}`;
+
+const redisClient = Redis.createClient({
+  url: redisUrl,
+  socket: {
+    tls: true, // Active l'utilisation de TLS
+    rejectUnauthorized: false // Pour les tests; mettre à true en production avec des certificats valides
   }
-})();
+});
 
-module.exports = redisClient;
+redisClient.on('error', (err) => console.error('Erreur de connexion Redis :', err));
+redisClient.connect().catch(console.error);
