9a2354fcfd0a59213e9b1054b65a993ca9b8bbd0
Hassane <bezzahassane@gmail.com>
2024-08-26 23:07:27 +0200

    home css

--- app/config/database.js
+++ app/config/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432, // Utilisez le port par défaut si non spécifié
+  port: process.env.DB_PORT || 5432, 
   dialect: 'postgres',
   logging: false,
 });
